f8c4fa496e5f21938f01ff895386cc3ae77a32d5
lin <lin>
2020-11-22 18:26:38 +0800

    class_08

--- class_07/class_07.py
+++ class_07/class_07.py
@@ -13,6 +13,62 @@ from howtrader.trader.object import TickData
 # Exchange.BINANCE
 # Interval.MINUTE
 
-tick = TickData()
+# tick = TickData()
 # tick.ask_volume_1
 # tick.bid_price_1
+
+# {'ask1': 100}  # key value
+
+
+# def hello(a):
+#     print(a)
+
+# int hello() {
+#
+#     return 10;
+# }
+
+
+def hello(string: str) -> int:
+    print(string)
+
+    return len(string)
+
+
+# a = hello("helloworld")
+#
+# print(a)
+
+# b = hello(10)
+# print(b)
+
+class Person(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+class Human(object):
+    def __init__(self, name, age):
+        self.name = name
+
+
+def func(person: Person) -> Person:
+    print(person.age, person.name)
+
+    return person
+
+a: Person = func(Person('lisi', 10))
+
+b = func(Person('lisi',15))
+
+print(a.name)
+print(a.age)
+
+print(b.age)
+
+from typing import Dict, Tuple, List
+
+def hi(a: List) -> List:
+
+    return a
--- class_07/class_7.md
+++ class_07/class_7.md
@@ -1,3 +1,43 @@
-#第七课: howtrader的常见类和配置
+# 第七课：VNPY的常见类的介绍和python类型编程
+
+## 导入类的方式
+
+```python
+
+from howtrader.A模块.文件 import 具体的某个类
+from howtrader.trader.object import Status  # 订单状态
+from howtrader.trader.object import OrderType
+from howtrader.trader.object import Exchange, Interval
+
+""" 
+实践的类型
+
+EVENT_TICK = "eTick."
+EVENT_TRADE = "eTrade."
+EVENT_ORDER = "eOrder."
+EVENT_POSITION = "ePosition."
+EVENT_ACCOUNT = "eAccount."
+EVENT_CONTRACT = "eContract."
+EVENT_LOG = "eLog"  
+EVENT_TIMER = "eTimer"
+"""
+
+# 配置文件： "vt_setting.json"
+
+```
+
+
+
+## python的类型编程
+
+```python
+
+from typing import Dict, Tuple, List
+
+def hello(a: int, b: int) -> int:
+    print(a, b)
+    return a+b
+
+```
 
 
