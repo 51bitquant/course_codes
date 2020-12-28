f38e86b84e0f8c97c73d3ed642bfeba0d9944044
lin <lin>
2020-12-28 13:40:25 +0800

    class_16

---
+++ class_16/grid.py
@@ -0,0 +1,47 @@
+# 网格交易的知识讲解
+
+"""
+655
+
+654
+
+653
+
+652
+
+651
+
+650
+
+649
+
+648
+
+647
+
+646
+
+645
+
+
+"""
+
+import numpy as np
+
+if __name__ == '__main__':
+
+    # prices = np.arange(660, 500, -3)
+    # print(prices)
+    #
+    # print(len(prices), prices.mean())
+
+    price1 = np.arange(660, 620, -3)
+
+    price2 = np.arange(620, 580, -5)
+
+    price3 = np.arange(580,500,  -10)
+
+    prices = list(price1) + list(price2) + list(price3)
+
+    p = np.array(prices)
+    print(len(p), p.mean())
